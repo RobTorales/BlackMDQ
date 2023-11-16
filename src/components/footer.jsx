05c0f1af983d11d324a0d7d179d591dcaa2aa372
roberto <roberto1608torales@gmail.com>
2023-11-15 21:29:40 -0300

    ultimos arreglos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,17 +6,19 @@ export const Footer = () => {
     <footer className="Footer">
         <div className="footer-div">
             <h4>Metodos de pago</h4>
-                <img src="../public/visa.png" alt="" className="tarjetas"/>
-                <img src="../public/cencosud@2x.png" alt="" className="tarjetas"/>
-                <img src="../public/cabal@2x.png" alt="" className="tarjetas"/>
-                <img src="../public/tarjeta-naranja@2x.png" alt="" className="tarjetas"/>
-                <img src="../public/banelco@2x.png" alt="" className="tarjetas"/>
-                <img src="../public/tarjeta-shopping@2x.png" alt="" className="tarjetas"/>
-                <img src="../public/a-express.png" alt="" className="tarjetas"/>
+                <img src="/img/visa.png" alt="" className="tarjetas"/>
+                <img src="/img/logo-mastercard.png" alt="" className="tarjetas"/>
+                <img src="/img/cencosud@2x.png" alt="" className="tarjetas"/>
+                <img src="/img/cabal@2x.png" alt="" className="tarjetas"/>
+                <img src="/img/tarjeta-naranja@2x.png" alt="" className="tarjetas"/>
+                <img src="/img/banelco@2x.png" alt="" className="tarjetas"/>
+                <img src="/img/tarjeta-shopping@2x.png" alt="" className="tarjetas"/>
+                <img src="/img/a-express.png" alt="" className="tarjetas"/>
         </div>
         <div className="footer-div">
             <h4>Formas de envío</h4>
-                <img src="../public/correo-argentino.png" alt="" className="tarjetas"/>
+                <img src="/img/correo-argentino.png" alt="" className="envio"/>
+                <img src="/img/Logo-oca.png" alt="" className="oca"/>
         </div>
         <div className="footer-div">
             <h4>Redes Sociales</h4>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,7 +8,7 @@ import './nav.css';
     return <header className="header">
                 <nav className="navbar navbar-expand-lg ">
                     <div className="container-fluid">
-                    <h1><img src="../public/rosa.png" alt="" />Black.Mdq<img src="../public/rosa.png" alt="" /></h1>
+                        <Link to="/"><img src="/img/logo-blackmdq.png" alt="" className="img-fluid"/></Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
